54a665297802ba00ca330dd8f6775039e5969b6d
PSAdmin <PSAdmin@DESKTOP-S7RLLPS>
2018-06-27 15:36:39 -0500

    first commit

---
+++ SpringMVC_Jeremy/src/main/java/com/perscholas/spring_mvc_lab/User.java
@@ -0,0 +1,73 @@
+package com.perscholas.spring_mvc_lab;
+
+public class User {
+
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String email;
+	private String city;
+	private String state;
+	
+{
+	
+}
+
+	public User(String firstName, String lastName, String phoneNumber, String email, String city, String state) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.city = city;
+		this.state = state;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
---
+++ SpringMVC_Jeremy/src/main/webapp/WEB-INF/views/form.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+   pageEncoding="ISO-8859-1"%>
+   <%@ taglib uri="http://java.sun.com/jstl/core" prefix="c"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"  %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+   
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Web Form</title>
+</head>
+<body>
+<form:form  method="POST" modelAttribute="newUser">
+    <form:errors path="*" />
+        <fieldset>
+           <legend>Add New User</legend>
+
+              <div class="form-group">
+               <label for="name">Name</label>
+              <div>
+                  <form:input id="firstName" path="firstName" type="text"/>
+                  <form:errors path="firstName"/>
+              </div>
+           </div>
+
+           
+           <div class="form-group">
+              <div>
+                 <input type="submit" id="btnAdd" value ="Add"/>
+              </div>
+           </div>
+        </fieldset>
+     </form:form>
+
+
+</body>
+</html>
